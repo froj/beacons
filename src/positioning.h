73df26ede56a4590313cb1daf367ddbee2a680cf
31415us <pius.vondaeniken@epfl.ch>
2014-07-02 22:21:29 +0200

    type identifier adapted to coding standard

--- src/positioning.h
+++ src/positioning.h
@@ -3,20 +3,20 @@
 #define POSITIONING_H
 
 #define POINT_A_X (0.0f)
-#define POINT_A_Y (1.0f)
+#define POINT_A_Y (0.0f)
 
 #define POINT_B_X (3.0f)
-#define POINT_B_Y (0.0f)
+#define POINT_B_Y (1.0f)
 
-#define POINT_C_X (3.0f)
+#define POINT_C_X (0.0f)
 #define POINT_C_Y (2.0f)
 
 typedef struct {
     float x;
     float y;
-} Position;
+} position_t;
 
-Position positioning_position_from_angles(float alpha, float beta, float gamma);
+position_t positioning_position_from_angles(float alpha, float beta, float gamma);
 
 #endif
 
