f252591b260220119e76f1135e9dcb8701b271fc
31415us <pius.vondaeniken@epfl.ch>
2014-09-20 22:51:13 +0200

    somewhere to put all the constants

---
+++ beacon_config.h
@@ -0,0 +1,12 @@
+
+#ifndef BEACON_CONFIG
+#define BEACON_CONFIG
+
+#define MAX_ACC (1.0f)
+#define NB_ROBOTS 2
+
+#define MEAS_VAR_X (1.0f)
+#define MEAS_VAR_Y (1.0f)
+#define MEAS_COV_XY (1.0f)
+
+#endif
