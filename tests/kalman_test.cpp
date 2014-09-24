9a97ba64296a8e928c0a6da6e296a9d4d79d5d70
31415us <pius.vondaeniken@epfl.ch>
2014-09-24 10:08:51 +0200

    test kalman measurement covariance update

--- tests/kalman_test.cpp
+++ tests/kalman_test.cpp
@@ -67,3 +67,44 @@ TEST(KalmanInitializationTestGroup, kalman_init)
     DOUBLES_EQUAL(MEAS_COV_XY, to_test._b, FLOAT_COMPARE_TOLERANCE);
     DOUBLES_EQUAL(MEAS_COV_XY, to_test._c, FLOAT_COMPARE_TOLERANCE);
 }
+
+TEST_GROUP(KalmanMeasurementCovUpdate)
+{
+    kalman_robot_handle_t handle;
+    void setup(void)
+    {
+        robot_pos_t init_pos;
+        init_pos.x = 0.0f;
+        init_pos.y = 0.0f;
+        init_pos.var_x = 1.0f;
+        init_pos.var_y = 1.0f;
+        init_pos.cov_xy = 0.0f;
+
+        kalman_init(&handle, &init_pos);
+    }
+
+    void teardown(void)
+    {
+
+    }
+};
+
+TEST(KalmanMeasurementCovUpdate, kalman_update_measurement_covariance)
+{
+    float new_var_x = 0.1f;
+    float new_var_y = 0.5f;
+    float new_cov_xy = 0.7f;
+
+    kalman_update_measurement_covariance(
+            &handle,
+            new_var_x,
+            new_var_y,
+            new_cov_xy);
+
+    // values should now equal new values
+    matrix2d_t to_test = handle._measurement_covariance;
+    DOUBLES_EQUAL(new_var_x, to_test._a, FLOAT_COMPARE_TOLERANCE);
+    DOUBLES_EQUAL(new_var_y, to_test._d, FLOAT_COMPARE_TOLERANCE);
+    DOUBLES_EQUAL(new_cov_xy, to_test._c, FLOAT_COMPARE_TOLERANCE);
+    DOUBLES_EQUAL(new_cov_xy, to_test._b, FLOAT_COMPARE_TOLERANCE);
+}
