87d5f044a9dabc8f4c5b6a2072a246b767e957ed
Florian Reinhard <florian.reinhard@epfl.ch>
2014-10-02 00:38:49 +0200

    positioning works with 1 laser

--- src/main.c
+++ src/main.c
@@ -14,6 +14,7 @@
 #include <platform-abstraction/timestamp.h>
 
 #include "beacon_angles.h"
+#include "positioning.h"
 
 void uart2_init(void)
 {
@@ -56,6 +57,26 @@ void exti_irq_init(void)
     exti_set_trigger(EXTI2, EXTI_TRIGGER_RISING);
     exti_select_source(EXTI2, GPIOD);
     nvic_enable_irq(NVIC_EXTI2_TSC_IRQ);
+    /*
+    // Setup for PB8 & PB9
+    exti_enable_request(EXTI8);
+    exti_set_trigger(EXTI8, EXTI_TRIGGER_RISING);
+    exti_select_source(EXTI8, GPIOB);
+    exti_enable_request(EXTI9);
+    exti_set_trigger(EXTI9, EXTI_TRIGGER_RISING);
+    exti_select_source(EXTI9, GPIOB);
+    nvic_enable_irq(NVIC_EXTI9_5_IRQ);
+    // Setup for PA10
+    exti_enable_request(EXTI10);
+    exti_set_trigger(EXTI10, EXTI_TRIGGER_RISING);
+    exti_select_source(EXTI10, GPIOD);
+    nvic_enable_irq(NVIC_EXTI15_10_IRQ);
+    */
+
+    // We'll also need to read the state of PB8 & PB9
+    rcc_periph_clock_enable(RCC_GPIOB);
+    gpio_mode_setup(GPIOB, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO8);
+    gpio_mode_setup(GPIOB, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO9);
 }
 
 
@@ -86,6 +107,11 @@ void fpu_config(void)
 beacon_angles_t laser_one;
 beacon_angles_t laser_two;
 
+position_t beacon_a = {1.5f, 0.5f};
+position_t beacon_b = {0.0f, 1.0f};
+position_t beacon_c = {0.0f, 0.0f};
+reference_triangle_t table;
+position_t laser_one_pos;
 
 os_thread_t laser_one_thread;
 THREAD_STACK laser_one_stack[1024];
@@ -104,8 +130,15 @@ void laser_one_main(void *context)
         if(beacon_angles_calculate(&laser_one)){
             gpio_toggle(GPIOB, GPIO13);
             os_mutex_take(&laser_one.access);
-            printf("alpha: %f, beta: %f, gamma: %f\n",
-                    DEG(laser_one.alpha), DEG(laser_one.beta), DEG(laser_one.gamma));
+            if(positioning_from_angles(
+                        laser_one.alpha,
+                        laser_one.gamma,
+                        laser_one.beta,
+                        &table, &laser_one_pos)){
+                printf("x: %f, y: %f\n\r", laser_one_pos.x, laser_one_pos.y);
+            }
+            //printf("alpha: %f, beta: %f, gamma: %f\n",
+            //        DEG(laser_one.alpha), DEG(laser_one.beta), DEG(laser_one.gamma));
             os_mutex_release(&laser_one.access);
         }
     }
@@ -123,6 +156,8 @@ int main(void)
     beacon_angles_init(&laser_two);
     beacon_angles_set_minimal_period(&laser_one, 50000);
     beacon_angles_set_minimal_period(&laser_two, 50000);
+
+    positioning_reference_triangle_from_points(&beacon_a, &beacon_b, &beacon_c, &table);
 
 
     exti_irq_init();
@@ -175,6 +210,13 @@ void exti9_5_isr(void)
     exti_reset_request(EXTI8);
     exti_reset_request(EXTI9);
 
+    if(gpio_get(GPIOB, GPIO8)){
+        beacon_angles_update_timestamp(&laser_two, A, os_timestamp_get());
+    }
+
+    if(gpio_get(GPIOB, GPIO9)){
+        beacon_angles_update_timestamp(&laser_two, B, os_timestamp_get());
+    }
 }
 
 // Beacon C, laser 2
@@ -182,5 +224,6 @@ void exti9_5_isr(void)
 void exti15_10_isr(void)
 {
     exti_reset_request(EXTI10);
+    beacon_angles_update_timestamp(&laser_two, C, os_timestamp_get());
 }
 
